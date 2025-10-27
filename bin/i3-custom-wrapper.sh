6df97c7276cd532d1f880edf2a53055527f83722
jerQ <jerq@jerq.fi>
2025-10-27 13:46:30 +0200

    Added comments to wrapper

--- bin/i3-custom-wrapper.sh
+++ bin/i3-custom-wrapper.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Wrapper script for 
+# /usr/share/xsessions/jerq.desktop
+#
+#[Desktop Entry]
+#Name=jerQ
+#Comment=
+#Exec=/home/jerq/bin/i3-custom-wrapper.sh
+#TryExec=i3
+#Type=Application
+#X-LightDM-DesktopName=i3
+#DesktopNames=i3
+#Keywords=tiling;wm;windowmanager;window;manager;
 
 # No terminal beeps
 xset -b
@@ -7,8 +19,6 @@ xset -b
 # CAPS2ESC
 setxkbmap -option caps:none
 setxkbmap -option caps:escape
-
-# 
 
 # RUN i3
 exec /usr/bin/i3 "$@"
